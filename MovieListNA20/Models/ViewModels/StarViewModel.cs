d0b3c92c7215dae26c9bdf31652b92eb11c969d3
DZM77 <d.bjorlingh@gmail.com>
2020-04-30 15:32:51 +0200

    Add ViewComponent

---
+++ MovieListNA20/Models/ViewModels/StarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieListNA20.Models.ViewModels
+{
+    public class StarViewModel
+    {
+        public int Stars { get; set; }
+        public bool IsHalfStar { get; set; }
+    }
+}
---
+++ MovieListNA20/ViewComponents/StarsViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieListNA20.Data;
+using MovieListNA20.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieListNA20.ViewComponents
+{
+    public class StarsViewComponent : ViewComponent
+    {
+        private readonly MovieListNA20Context context;
+
+        public StarsViewComponent(MovieListNA20Context context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int movieId)
+        {
+            var movie = await context.Movie.FindAsync(movieId);
+
+            var doubleRating = (int)Math.Round(movie.Rating * 2);
+
+            var model = new StarViewModel()
+            {
+                Stars = doubleRating / 2,
+                IsHalfStar = doubleRating % 2 == 1
+            };
+
+            return View(model);
+        }
+    }
+}
